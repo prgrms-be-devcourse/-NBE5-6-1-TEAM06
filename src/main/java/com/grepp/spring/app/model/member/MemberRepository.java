96edd728ba03f7459b36c80dca9b1314d81e3583
ash373758 <ash373758@gmail.com>
2025-04-23 12:40:49 +0900

    branch test

---
+++ src/main/java/com/grepp/spring/app/model/member/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.grepp.spring.app.model.member;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface MemberRepository {
+
+}
