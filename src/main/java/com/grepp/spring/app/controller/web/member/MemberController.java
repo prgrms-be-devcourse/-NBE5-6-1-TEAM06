dd37246670d438ac67fa81b33ec365b166d24c0f
ash373758 <ash373758@gmail.com>
2025-04-25 00:58:13 +0900

    fix

--- src/main/java/com/grepp/spring/app/controller/web/member/MemberController.java
+++ src/main/java/com/grepp/spring/app/controller/web/member/MemberController.java
@@ -7,17 +7,14 @@ import com.grepp.spring.app.model.member.MemberService;
 import com.grepp.spring.app.model.member.dto.Member;
 import com.grepp.spring.app.model.order.OrderService;
 import jakarta.servlet.http.HttpSession;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
